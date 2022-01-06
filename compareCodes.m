1cd96c9845c231beafd164bccb36f25463b75db3
maya-e-s <76231198+maya-e-s@users.noreply.github.com>
2022-01-05 21:43:27 -0500

    Update compareCodes.m

--- compareCodes.m
+++ compareCodes.m
@@ -1,4 +1,9 @@
 function [rw] = compareCodes(a, b)
+% function compareCodes: compares sets A and B using the the number of red and white pegs that would be returned if the codebreaker guessed code ‘b’ while the secret code was code ‘a’ 
+% inputs: a - a four number code representing the possible secret code
+%         b - a four number code representing a potentional codebreaker guess
+% output:rw - set containing [red, white] peg combo corresponing with guesses 'a' and 'b'
+    
     rw = [0 0]; % default
 
     % check red pegs
