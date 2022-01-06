4ac378d52fd9d8272c0158a3c8b79780253b7b43
maya-e-s <76231198+maya-e-s@users.noreply.github.com>
2022-01-05 21:41:09 -0500

    Update nextGuess.m

--- nextGuess.m
+++ nextGuess.m
@@ -1,7 +1,15 @@
 function [guess, A, B] = nextGuess(A, B)
+% function nextGuess: returns next guess computer should make by using the minimax technique (minimizes maximum loss)
+% inputs: A - set containing all unused combinations. Every time a code is used as a guess, that code is eliminated from A
+%         B - set containing all possible codes that may match the secret code created by the codemaker 
+% outputs:guess - four numbers representing the next guess the computer should make
+%         A - updated set A
+%         B - updated set B
+    
     [rA cA] = size(A); 
     [rB cB] = size(B);
     scores = zeros(14, rA); % number of codes that will not be eliminated for every element in A (codes remaining)
+    % Every element in set A is compared to those remaining in B, and the number of codes that would remain in B given each combination of possible red and white return pegs is stored in a new set called ‘scores’
     for i=1:1:rA % for each unused guess (A)
         for j=1:1:rB % for each possible code left (B)
             rw = compareCodes(A(i, :), B(j, :)); 
@@ -37,13 +45,18 @@ function [guess, A, B] = nextGuess(A, B)
         end
     end
 
+    % The maximum score out of each of the five scores for each element in A represents the worse case scenario, or having that amount of codes remain in B after that guess is played. 
     maxScores = zeros(1, rA); % max of each column of scores (worst case scenario)
     for i=1:1:rA
         maxScores(i) = max(scores(:, i));
     end
 
+    % The minimum element in maxScores shows the minimum amount of codes that can remain in B following the worst case scenario
     miniMax = min(maxScores); % min of maxScores (best of worst case scenario)
 
+    % Any element of A whose maxScore matches miniMax is added to a new set called ‘nextGuess’
+    % If an element of nextGuess also exists in B, that guess is played. 
+    % If none of the elements of nextGuess exist in B, then the first element in nextGuess is played
     nextGuess = zeros(1, 4); % possible next guesses
     j=1;
     for i=1:1:rA
@@ -55,7 +68,6 @@ function [guess, A, B] = nextGuess(A, B)
             j = j+1;
         end
     end
-
     [rNG cNG] = size(nextGuess);
     inB = ismember(nextGuess, B, 'rows');
     guess = [0 0 0 0]; % pick next guess
